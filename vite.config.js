8a7143b5e56bccc3ba011fc729e764dbfa0441f8
stvnmc <stvnmc123@gmail.com>
2024-01-23 10:04:11 -0600

    .env

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/Spotify/', // Ajusta el valor según tu configuración de GitHub Pages
+  build: {
+    outDir: 'dist', // Directorio de salida para archivos construidos
+    assetsDir: '', // Directorio para los activos (assets), ajusta según tu estructura de carpetas
+    manifest: true, // Generar archivo manifest.json
+  },
+});
